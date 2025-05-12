8c378d2b8fe4746b52d13202f5f89b6f5ec971a9
Aditya Goyal <adityamr.1694@gmail.com>
2025-05-13 00:24:11 +0530

    added comments for code readability

--- backend/src/docs/swagger.js
+++ backend/src/docs/swagger.js
@@ -2,9 +2,11 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
 
+// Load Swagger specification from YAML file
 const swaggerDocument = YAML.load(path.resolve(__dirname, 'swagger.yaml'));
 
 function setupSwagger(app) {
+  // Mount Swagger UI at /api-docs
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
